da803d2cbeebd777e09def1922cd36b064956800
Benjamin Chilson <benjameschilson@gmail.com>
2020-06-08 09:39:48 -0500

    Added singleton elements to the shop class

--- src/sandwichmaker/Shop.java
+++ src/sandwichmaker/Shop.java
@@ -1,5 +1,23 @@
 package sandwichmaker;
 
 public class Shop {
-
+	
+	private static Shop shop;
+	private SandwichMenu sandwichMenu;
+	private DrinkMenu drinkMenu;
+	
+	
+	private Shop() {
+		sandwichMenu = new SandwichMenu();
+		drinkMenu = new DrinkMenu();
+	}
+	
+	public static Shop getShop() {
+		if(shop == null)
+			shop = new Shop();
+		
+		return shop;
+	}
+	
+	
 }
